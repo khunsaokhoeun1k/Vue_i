56ee53cb3d8a8d20c7c9bfb033273f143c77c4c4
khunsaokhoeun1k <saokhoeunk1606@gmail.com>
2024-10-18 16:16:13 +0700

    Test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/laravel.png">
+  <!-- <HelloWorld msg="This is new Project"/> -->
+  <ChildComponent message ="I love your combodai"/>
+  <!-- <ChildComponent message ="Hello Komphong speu"/> -->
+  <!-- <ChildComponent :message ="msg" type = "ok"/> -->
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChildComponent
+  },
+  // data(){
+  //   return{
+  //     msg:'Good Morning',
+  //   }
+  // }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{message}}</h1>
+    <!-- <h2>{{ type }}</h2> -->
+    <p>
+      For a guide and recipes on how to configure / My  project,<br>
+      check out the
+      <a href="#">laravel documentation</a>.
+    </p>
+    <h2>Installe documentation </h2>
+</template>
+
+<script>
+ export default {
+  name: 'ChildComponent',
+  props: {
+    message:String,
+    // type:String
+  }
+}
+</script>
